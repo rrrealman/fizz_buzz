d32d188d63196ea919a0ba29e24bd41b88296f37
sw <shlackbox@yandex.ru>
2017-03-18 12:33:03 +0300

    Refactored max_substrs_len.py

--- source/max_substrs_len.py
+++ source/max_substrs_len.py
@@ -1,33 +1,52 @@
+from collections import namedtuple
 
-def count_max_substr(string):
+
+def max_len_substr(string):
 
     '''
         This routine returns the longest sequences
         of character founded in string
     '''
 
-    res = {}
-
-    if len(string) == 0:
-        return res
-
-    def update_res(res, char, counter):
-        if char not in res or res[char] < counter:
-            res[char] = counter
-
-    char_counter = 1
+    counter = 1
     prev_char = string[0]
+    chars = {}
+
     for char in string[1:]:
+
         if char != prev_char:
-            update_res(res, prev_char, char_counter)
-            char_counter = 1
+
+            if prev_char not in chars or chars[prev_char] < counter:
+                chars[prev_char] = counter
+
+            counter = 1
+
         else:
-            char_counter += 1
+
+            counter += 1
+
         prev_char = char
-    update_res(res, prev_char, char_counter)
-    return res
+
+    if prev_char not in chars or chars[prev_char] < counter:
+        chars[prev_char] = counter
+
+    return chars
 
 
-test_string = 'bbbaaaaabsssssssssddddddffffffffeeeeew'
-print(count_max_substr(test_string))
-# {'d': 6, 's': 9, 'b': 3, 'e': 5, 'a': 5, 'w': 1, 'f': 8}
+if __name__ == '__main__':
+
+    TestCase = namedtuple('TestCase', ['data', 'ref'])
+
+    test_cases = [TestCase('ababb', {'a': 1, 'b': 2}),
+                  TestCase('abbab', {'a': 1, 'b': 2}),
+                  TestCase('aababb', {'a': 2, 'b': 2}),
+                  TestCase('abbaabba', {'a': 2, 'b': 2})]
+
+    for test_case in test_cases:
+        chars = max_len_substr(test_case.data)
+
+        print(
+            'Test data: {}\ttest reference: {}\ttest result: {}'.format(
+                test_case.data, test_case.ref, chars))
+
+        assert test_case.ref == chars
